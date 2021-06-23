4f6c239ffd0fa8126dc37e168f515451cc5136a9
Miyakawa Takuya <omyakawa@gmail.com>
2021-06-23 18:16:26 +0900

    初回投稿

---
+++ get_file.php
@@ -0,0 +1,21 @@
+<?php
+// var_dump($_POST);
+var_dump($_FILE);
+exit();
+
+// $picture = $_POST["imageStr"];
+// $folder_path = "images/" . time() . ".png";
+
+// $data = str_replace('data:image/png;base64,', '', $picture);
+// $data = str_replace(' ', '+', $data);
+// $data = base64_decode($data);
+// file_put_contents($folder_path, $data);
+
+// $conn = mysqli_connect("localhost:8889", "root", "root", "tutorials");
+
+// $sql = "INSERT INTO images (path) VALUES ('$folder_path')";
+// mysqli_query($conn, $sql);
+
+// echo "Image has been uploaded";
+
+?>
